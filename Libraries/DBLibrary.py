d6fecdb94724f7e957842874dcf8916fcabb7843
Maddy <maddyvn@gmail.com>
2016-06-28 00:55:49 +0700

    Optimize library

--- Libraries/DBLibrary.py
+++ Libraries/DBLibrary.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+#_*_ coding: utf-8
 
 import pymssql
 import csv
 from xlsxwriter import Workbook
+from xlsxwriter.format import Format
 from collections import defaultdict
 from robot.api import logger
 
 class DBLibrary(object):
-	"""
+	'''
 		Database Library for Robot-Framework
 			Prequisite:
 				- xlsxwriter (Excel file library) #http://xlsxwriter.readthedocs.io/index.html
@@ -17,34 +18,31 @@ class DBLibrary(object):
 			Install from cmd:
 				pip install pymssql==2.1.1
 				pip install xlsxwriter
-	"""
+	'''
 	
-	def __init__(self):
-		self._dbconnection = None	#Database connection
-	
+	def __init__(self): self._dbconnection = None	#Database connection
+	def _encode_val(self, s): return unicode(s).encode("utf-8")
 	def connect_to_database(self, dbServer, dbUser, dbPass, dbDatabase, dbPort=1433, dbQTimeout=0, dbLoginTimeout=60, charSet='UTF-8'):
-		"""
+		'''
 			Connect to database. Must be called before calling any query actions.
-		"""
-	
+		'''
 		try:
 			self._dbconnection = pymssql.connect(server=dbServer, user=dbUser, password=dbPass, database=dbDatabase, port=int(dbPort), timeout=int(dbQTimeout), login_timeout=int(dbLoginTimeout), charset=charSet, as_dict=False)
 		except Exception as e:
 			logger.error(e)
 	
 	def connect_to_database_by_cfFile(self, filename):
-		"""
+		'''
 			Connect to database. Must be called before calling any query actions.
 			Format:
-				dbServer=
-				dbPort=
-				dbUser=
-				dbPassword=
-				dbDataBase=
-				dbQueryTimeout=0
-				dbLoginTimeout=60
-		"""
-		
+				dbServer=@value\n
+				dbPort=@value\n
+				dbUser=@value\n
+				dbPassword=@value\n
+				dbDataBase=@value\n
+				dbQueryTimeout=0\n
+				dbLoginTimeout=60\n
+		'''
 		dict = {}
 		f = open(filename)
 		for lines in f:
@@ -54,36 +52,38 @@ class DBLibrary(object):
 		self.connect_to_database(dict.get('dbServer'), dict.get('dbUser'), dict.get('dbPassword'), dict.get('dbDataBase'), dict.get('dbPort'), dict.get('dbQueryTimeout'), dict.get('dbLoginTimeout'))
 	
 	def disconnect_from_database(self):
-		"""
+		'''
 			Disconnect from current database connection. Should be called after finishing all query actions.
-		"""
+		'''
 		self._dbconnection.close()
 	
-	def get_query_result_csv(self, sql, csvFile):
-		"""
+	def get_query_result_csv(self, sql, csvFile, header=True):
+		'''
 			Build a csv file from result set of the sql command \n
 			Example: get query result csv		select * from customer		test.csv
-		"""
+		'''
 		
 		cur = self._dbconnection.cursor()
 		cur.execute(sql + ';')
 		rows = cur.fetchall()
 		
-		columns = []
-		for column in cur.description:
-			columns.append(column[0])
-		
 		with open(csvFile, "wb") as outfile:
 			writer = csv.writer(outfile)
-			writer.writerow(columns)
-			for row in rows:
-				writer.writerow([unicode(s).encode("utf-8") for s in row])
-				
-	def get_query_result_excel(self, sql, excelFile):
-		"""
+			if header: writer.writerow([column[0] for column in cur.description])	#Write header column
+			for row in rows: writer.writerow([unicode(s).encode("utf-8") for s in row])
+	
+	def format_list_to_string(self, list, type='Number'):
+		'''
+			Return a string from a list with sql formated. Useful for IN condition
+		'''
+		if type == 'Number': return str(list).strip('[]').replace('u','').replace('\'','').replace(' ','')
+		else: return str(list).strip('[]').replace('u','').replace(' ','')
+	
+	def get_query_result_excel(self, sql, excelFile, header=True):
+		'''
 			Build an excel file from result set of the sql command \n
 			Example: get query result excel		select * from customer		test.xlsx
-		"""
+		'''
 		
 		cur = self._dbconnection.cursor()
 		cur.execute(sql + ';')
@@ -91,19 +91,32 @@ class DBLibrary(object):
 		
 		workbook = Workbook(excelFile)
 		sheet = workbook.add_worksheet()
+		format = workbook.add_format({'bold': True})
 		
-		# Get columns list
-		columns = []
-		for column in cur.description:
-			columns.append(column[0])
-		
-		# Write header column
-		for i, val in enumerate(columns):
-			sheet.write(0, i, val)
-		
-		# Write table data
+		if header:
+			for i, val in enumerate([column[0] for column in cur.description]): sheet.write(0, i, self._encode_val(val), format)	#Write header column
 		for r, row in enumerate(rows):
-			for c, col in enumerate(row):
-				sheet.write(r+1, c, unicode(col).encode('utf-8'))
-		
+			for c, cell in enumerate(row): sheet.write(r+1, c, cell) # Write table data
 		workbook.close()
+	
+	def get_query_result_dict(self, sql):
+		'''
+			Return a list of dictionary which stands for each row in queried table
+		'''
+		cur = self._dbconnection.cursor()
+		cur.execute(sql + ';')
+		rows = cur.fetchall()
+		return [dict(zip([column[0] for column in cur.description], map(self._encode_val, row))) for row in rows]
+		
+	def get_query_result_list(self, sql, header=True):
+		'''
+			Return a list of list which stands for each row in queried table
+		'''
+		cur = self._dbconnection.cursor()
+		cur.execute(sql + ';')
+		rows = cur.fetchall()
+		
+		data = []
+		if header: data.append([column[0] for column in cur.description]) 	#Write header column
+		for row in rows: data.append(row)
+		return data
--- Libraries/Selenium2Extend.py
+++ Libraries/Selenium2Extend.py
@@ -1,9 +1,11 @@
 from robot.libraries.BuiltIn import BuiltIn
 from robot.api import logger
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
 
 '''
 	Extend the Selenium2Library with customized methods for checking element state (it is check, not assert)
@@ -16,99 +18,96 @@ from selenium.webdriver.support import expected_conditions as EC
 	Version: 1.0
 '''
 
-class myElement(object):
+class Element(object):
 	def __init__(self, locator):
-		address = str(locator).split('=', 1)
-		self._elem = address[0], address[1]	#By.class, value
-		self._validate_locator()
+		self._validate(locator)
 		
-	def _validate_locator(self):
-		if not By.is_valid(self._elem[0]): raise Exception('ERROR: Wrong definion of locator: ' + self._elem[0] + '=' + self._elem[1])
+	def _validate(self, locator):
+		s = str(locator).split('=', 1)
+		if s[0] not in ['id','name','xpath','tag name','partial link text','link text','css selector','class name']:
+			raise ValueError('Wrong identifier definion of locator: ' + s[0] + '=' + s[1])
+		self._elem = s[0], s[1]	#By.class, value
 	
-	def _myElem(self): return self._elem
+	def _get(self): return self._elem
 
 def _driver(): return BuiltIn().get_library_instance('Selenium2Library')._current_browser()
+def _action(): return ActionChains(_driver())
 def _timeOut(s): return 0 if int(s) < 0 else int(s)
 def _wait(s): return WebDriverWait(_driver(), _timeOut(s))
+def _findElem(locator):
+	elem = Element(locator)._get()
+	return _driver().find_element(elem[0],elem[1])
 
 ##################### Verify element conditions ##################
 ##################################################################
 
-def is_element_visible(locator, timeOut=0):
-	'''
-		An expectation for checking that an element is present on the DOM of a page and visible.\n
-		Visibility means that the element is not only displayed but also has a height and width that is greater than 0.\n
-		Locator - used to find the element returns True once it is located and visible
-	'''
-	logger.debug('Check if element ' + locator + ' is visible for ' + str(_timeOut(timeOut)) + ' seconds')
-	try: _wait(timeOut).until(EC.visibility_of_element_located(myElement(locator)._myElem()))
-	except Exception: return False
-	return True
-	
 def does_element_exists(locator, timeOut=0):
 	'''
-		An expectation for checking that an element is present on the DOM of a page.\n
+		Checking that an element is present on the DOM of a page.\n
 		This does not necessarily mean that the element is visible.\n
 		Locator - used to find the element returns True once it is located
 	'''
-	logger.debug('Check if element ' + locator + ' exists for ' + str(_timeOut(timeOut)) + ' seconds')
-	try: _wait(timeOut).until(EC.presence_of_element_located(myElement(locator)._myElem()))
-	except Exception: return False
+	try: _findElem(locator)
+	except WebDriverException: return False
 	return True
 
-def is_element_clickable(locator, timeOut=0):
+def is_element_visible(locator, timeOut=0):
 	'''
-		An Expectation for checking an element is visible and enabled such that you can click it.
+		Checking that an element is present on the DOM of a page and visible.\n
+		Visibility means that the element is not only displayed but also has a height and width that is greater than 0.\n
+		Locator - used to find the element returns True once it is located and visible
 	'''
-	logger.debug('Check if element ' + locator + ' is enabled for ' + str(_timeOut(timeOut)) + ' seconds')
-	try: _wait(timeOut).until(EC.element_to_be_clickable(myElement(locator)._myElem()))
-	except Exception: return False
+	try: _wait(timeOut).until(EC.visibility_of_element_located(Element(locator)._get()))
+	except WebDriverException: return False
 	return True
 
 def is_element_invisible(locator, timeOut=0):
 	'''
-		An Expectation for checking that an element is either invisible or not present on the DOM.\n
+		Checking that an element is either invisible or not present on the DOM.\n
 		Locator used to find the element
 	'''
-	logger.debug('Check if element ' + locator + ' is invisible for ' + str(_timeOut(timeOut)) + ' seconds')
-	try: _wait(timeOut).until(EC.invisibility_of_element_located(myElement(locator)._myElem()))
-	except Exception: return False
+	try: _wait(timeOut).until(EC.invisibility_of_element_located(Element(locator)._get()))
+	except WebDriverException: return False
+	return True
+
+def is_element_clickable(locator, timeOut=0):
+	'''
+		Checking an element is visible and enabled such that you can click it.
+	'''
+	try: _wait(timeOut).until(EC.element_to_be_clickable(Element(locator)._get()))
+	except WebDriverException: return False
 	return True
 
 def is_element_selected(locator, timeOut=0):
 	'''
-		An expectation for the element to be located is selected
+		Check whether the element to be located is selected
 	'''
-	logger.debug('Check if element ' + locator + ' is invisible for ' + str(_timeOut(timeOut)) + ' seconds')
-	try: return _wait(timeOut).until(EC.element_located_to_be_selected(myElement(locator)._myElem()))
-	except Exception: return False
+	try: return _wait(timeOut).until(EC.element_located_to_be_selected(Element(locator)._get()))
+	except WebDriverException: return False
 
 def is_text_present_in_element(locator, text, timeOut=0):
 	'''
-		An expectation for checking if the given text is present in the specified element\n
+		Checking if the given text is present in the specified element\n
 	'''
-	logger.debug('Check if a text is present in element ' + locator + ' for ' + str(_timeOut(timeOut)) + ' seconds')
-	try: _wait(timeOut).until(EC.text_to_be_present_in_element_value(myElement(locator)._myElem(), text))
-	except Exception: return False
+	try: _wait(timeOut).until(EC.text_to_be_present_in_element_value(Element(locator)._get(), text))
+	except WebDriverException: return False
 	return True
 
 def is_alert_present(timeOut=0):
 	'''
-		Expect an alert to be present.\n
+		Check whether an alert to be present.\n
 	'''
-	logger.debug('Check if an alert is present for ' + str(_timeOut(timeOut)) + ' seconds')
 	try: _wait(timeOut).until(EC.alert_is_present())
-	except Exception: return False
+	except WebDriverException: return False
 	return True
 
 def is_frame_available(locator, timeOut=0):
 	'''
-		An expectation for checking whether the given frame is available to switch to.\n
+		Checking whether the given frame is available to switch to.\n
 		If the frame is available it switches the given driver to the specified frame
 	'''
-	logger.debug('Check if the frame ' + locator + ' is available to switch to it for ' + str(_timeOut(timeOut)) + ' seconds')
-	try: _wait(timeOut).until(EC.frame_to_be_available_and_switch_to_it(myElement(locator)._myElem()))
-	except Exception: return False
+	try: _wait(timeOut).until(EC.frame_to_be_available_and_switch_to_it(Element(locator)._get()))
+	except WebDriverException: return False
 	return True
 
 def is_title_equals(text, timeOut=0):
@@ -116,13 +115,48 @@ def is_title_equals(text, timeOut=0):
 		An expectation for checking that the title contains a case-sensitive substring. \n
 		Title is the fragment of title expected returns True when the title matches, False otherwise
 	'''
-	logger.debug('Check if the page title equals to: "' + text + '" for ' + str(_timeOut(timeOut)) + ' seconds')
 	return _wait(timeOut).until(EC.title_is(text))
 	
 def is_title_contains(text, timeOut=0):
 	'''
-		An expectation for checking the title of a page.\n
+		Checking the title of a page.\n
 		Title is the expected title, which must be an exact match returns True if the title matches, false otherwise.
 	'''
-	logger.debug('Check if the page title contains case-sensitive substring: "' + text + '" for ' + str(_timeOut(timeOut)) + ' seconds')
 	return _wait(timeOut).until(EC.title_contains(text))
+	
+def move_to_element(locator):
+	'''
+		Focus to the element by locator.
+	'''
+	_findElem(locator).send_keys(Keys.NULL)
+
+##################### Keyboard input ##################
+#######################################################
+		
+def key_input(key, locator=None):
+	'''
+		Sends keys to element. If no element is specified then send key to focused element
+	'''
+	if locator != None: move_to_element(locator)
+	_action().send_keys(key).perform()
+		
+def key_paste(locator=None):
+	'''
+		Perform Ctrl + V to paste from a clipboard value. If no element is specified then execute on focused element
+	'''
+	if locator != None: move_to_element(locator)
+	_action().key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+	
+def key_select_all(locator=None):
+	'''
+		Perform Ctrl + A to select all content of focused element. If no element is specified then execute on focused area
+	'''
+	if locator != None: move_to_element(locator)
+	_action().key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).perform()
+	
+def key_copy(locator=None):
+	'''
+		Perform Ctrl + C to copy selected content of an element. If no element is specified then execute on focused element
+	'''
+	if locator != None: move_to_element(locator)
+	_action().key_down(Keys.CONTROL).send_keys('c').key_up(Keys.CONTROL).perform()
